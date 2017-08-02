6255b82762ccdd46bce390ad3a350772a695e5c9
COOSK-KUNKUN <532866709@qq.com>
2017-08-02 13:24:15 +0800

    1

--- test_thread2.py
+++ test_thread2.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
+# 线程同步
 
 import threading
 import time
 
-exitFlag = 0
+'''exitFlag = 0
 #  exitflag参数中的数字：0、到达最大迭代次数或到达函数评价
+'''
 
 class mythread(threading.Thread):
 	"""docstring for mythread"""
@@ -21,22 +23,30 @@ class mythread(threading.Thread):
         # 参考 http://python.jobbole.com/81921/
 
 	def run(self):
-		# #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
+		# 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
 		print 'staring ' + self.name
-		print_time(self.name, self.counter, 5)
+		threadLock.acquire()
+		# 获得锁，成功获得锁定后返回True
+		# 可选的timeout参数不填时将一直阻塞直到获得锁定
+		# 否则超时后将返回False
+		print_time(self.name, self.counter, 3)
 		print 'exiting ' + self.name
+		threadLock.release()
 		pass
 
 def print_time(threadName, delay, counter):
 	while counter:
-		if exitFlag:
-			thread.exit()
-			time.sleep(delay)
-			print 'the time is %s: %s' % (threadName,time.ctime(time.time()))
-			counter -= 1
-			pass
-		pass
+			'''if exitFlag:'''
+			'''thread.exit()'''
+	time.sleep(delay)
+	# 休眠                        
+	print 'the time is %s: %s' % (threadName,time.ctime(time.time()))
+	counter -= 1
+	pass
+	pass
 				
+threadLock = threading.Lock()
+threads = []
 
 # 创建新线程
 thread1 = mythread(1, "Thread-1", 1)
@@ -45,5 +55,13 @@ thread2 = mythread(2, "Thread-2", 2)
 # 开启线程
 thread1.start()
 thread2.start()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
 
 print "\nExiting Main Thread"
